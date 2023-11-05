b20068cd16d9fe07475307113486a68462eb070d
logan9997 <80635281+logan9997@users.noreply.github.com>
2023-11-05 18:41:43 +0000

    database connectionconfigured to connect to postgres database

--- LegoMarket/settings.py
+++ LegoMarket/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+from environment_manager import Manager
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -75,10 +76,7 @@ WSGI_APPLICATION = 'LegoMarket.wsgi.application'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': Manager.get_database_credentails('settings')
 }
 
 
