6cf5b9e45290a4404f1b892ac7339e99d24ca604
logan9997 <80635281+logan9997@users.noreply.github.com>
2023-11-05 15:02:58 +0000

    ModelConfig added; created constants for Choices, Maxlengths, Decimals

---
+++ config.py
@@ -0,0 +1,15 @@
+class ModelsConfig:
+
+    class Length:
+        ITEM_ID = 50
+        ITEM_NAME = 300
+        ITEM_TYPE = 1
+        USERNAME = 16
+        PASSWORD = 20
+
+    class Decimal:
+        MAX_DIGITS = 10
+        DECIMAL_PLACE = 2
+
+    class Choice:
+        ITEM_TYPE = (('S', 'S'), ('M', 'M'))
