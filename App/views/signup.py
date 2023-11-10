72f66a24eb948584baba4be2aa8df5b5b4afbebb
logan9997 <80635281+logan9997@users.noreply.github.com>
2023-11-10 19:41:06 +0000

    successfully redirects when form is valid

--- App/views/signup.py
+++ App/views/signup.py
@@ -12,18 +12,19 @@ class SignUpView(FormView):
     form_class = SignUp
             
     def post(self, request: HttpRequest, *args: str, **kwargs: Any) -> HttpResponse:
-        form = SignUp(request.POST)
-        if form.is_valid():
-            username = form.data.get('username')
-            password = form.data.get('password')
-
-            new_user = User.objects.create(
-                username=username,
-                password=password
-            )
-            new_user.save()
-            request.session['user_id'] = new_user.user_id
+        self.request = request
+        SignUp(request.POST)
         return super().post(request, *args, **kwargs)
     
-    
+    def form_valid(self, form: Any) -> HttpResponse:
+        username = form.data.get('username')
+        password = form.data.get('password')
 
+        new_user = User.objects.create(
+            username=username,
+            password=password
+        )
+        new_user.save()
+        self.request.session['user_id'] = new_user.user_id
+        return super().form_valid(form)
+    
