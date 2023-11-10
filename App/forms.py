4210f589757cb842d21c99b9ee5d5734e8f23629
logan9997 <80635281+logan9997@users.noreply.github.com>
2023-11-10 19:01:12 +0000

    SignUp form created

--- App/forms.py
+++ App/forms.py
@@ -25,5 +25,29 @@ class Login(forms.Form):
             return True
         return False
     
-    def get_user_id(self):
+    def get_user_id(self) -> int:
         return self.user.get().user_id
+
+
+class SignUp(forms.Form):
+    username = forms.CharField(
+        max_length=ModelsConfig.Length.USERNAME
+    )
+    password = forms.CharField(
+        max_length=ModelsConfig.Length.PASSWORD,
+        widget=forms.PasswordInput()
+    )
+    confirm_password = forms.CharField(
+        max_length=ModelsConfig.Length.PASSWORD,
+        widget=forms.PasswordInput()
+    )
+
+    def is_valid(self) -> bool:
+        username = self.data.get('username')
+        password = self.data.get('password')
+        confirm_password = self.data.get('confirm_password')
+
+        username_taken = User.objects.filter(username=username).exists()
+        if password == confirm_password and not username_taken:
+            return True
+        return False
