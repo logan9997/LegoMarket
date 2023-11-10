045076d26db5b3495049fb9865df4a9b180935b1
logan9997 <80635281+logan9997@users.noreply.github.com>
2023-11-10 19:01:29 +0000

    signup url configured

--- App/urls.py
+++ App/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 from .views import (
-    home, item, login
+    home, item, login, signup
 )
 
 urlpatterns = [
     path('', home.home, name='home'),
     path('item/<str:item_id>/', item.ItemView.as_view(), name='item'),
-    path('login/', login.LoginView.as_view(), name='login')
+    path('login/', login.LoginView.as_view(), name='login'),
+    path('signup/', signup.SignUpView.as_view(), name='signup'),
+
 ]
