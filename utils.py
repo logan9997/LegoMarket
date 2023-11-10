dca638f3d036e727e0a3dd7faae938e112f604ff
logan9997 <80635281+logan9997@users.noreply.github.com>
2023-11-10 14:43:05 +0000

    added doc strings to all functions, added excpetion block inside item_type_convert, created timer func

--- utils.py
+++ utils.py
@@ -1,14 +1,26 @@
+from typing import Any
+import time
 
-def item_type_convert(item_type):
+def item_type_convert(item_type:str) -> str:
+    '''
+    Converts item type to alternative format
+    '''
     types = {
         'M':'MINIFIG',
         'MINIFIG':'M',
         'S':'SET',
         'SET':'S'
     }
+
+    if item_type not in types.keys():
+        raise Exception('Invalid item type')
+
     return types[item_type]
 
 def clean_html_codes(string:str) -> str:
+    '''
+    Removes html codes from string; e.g. "&#40Anakin Skywalker&#41" -> "(Anakin Skywalker)"
+    '''
     codes = {
         '&#41;': ')',
         '&#40;': '(',
@@ -18,3 +30,15 @@ def clean_html_codes(string:str) -> str:
         if html in string:
             string = string.replace(html, char)
     return string
+
+def timer(func: Any) -> Any:
+    '''
+    Calculates run time of a function
+    '''
+    def wrapper(*args, **kwargs) -> Any:
+        start = time.time()
+        result = func(*args, **kwargs)
+        finish = time.time() - start
+        print(f'<{func.__name__}> finished in {round(finish, 5)} seconds.')
+        return result
+    return wrapper
