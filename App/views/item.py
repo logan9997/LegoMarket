49705c46c1f70bb8604a19555be60c0a1afc13c6
logan9997 <80635281+logan9997@users.noreply.github.com>
2023-11-10 18:49:05 +0000

    created methods for get_selected_graph_metric, get_metric_percentage_change

--- App/views/item.py
+++ App/views/item.py
@@ -14,7 +14,7 @@ class ItemView(TemplateView):
     def dispatch(self, request: HttpRequest, item_id, *args: Any, **kwargs: Any) -> HttpResponse:
         self.item_id = item_id
         self.title = f'Item/{self.item_id}'
-        self.selected_graph_metric = 'price_new'
+        self.selected_graph_metric = self.get_selected_graph_metric()
         self.request = request
         return super().dispatch(request, *args, **kwargs)
 
@@ -23,21 +23,24 @@ class ItemView(TemplateView):
         context.update({
             'title': self.title,
             'item_info': self.get_item_info(),
-            'graph_metrics': self.get_graph_metrics(self.selected_graph_metric),
+            'graph_metrics': self.get_graph_data(self.selected_graph_metric),
             'price_new': self.get_current_metric('price_new'),
             'price_used': self.get_current_metric('price_used'),
             'qty_new': self.get_current_metric('qty_new'),
             'qty_used': self.get_current_metric('qty_used'),
             'similar_items': self.get_similar_items(),
-            'form':GraphMetricSelect
+            'form':GraphMetricSelect,
         })
-        self.get_similar_items()
+        print(self.get_percentage_change('price_new'))
         return context
+    
+    def get_selected_graph_metric(self) -> str:
+        return self.request.GET.get('metric_select', 'price_new')
     
     def get_item_info(self):
         return Item.objects.get(item_id=self.item_id)
     
-    def get_graph_metrics(self, metric:str):
+    def get_graph_data(self, metric:str):
         metrics = Price.objects.filter(
             item_id=self.item_id
         ).values('date', metric).order_by('-date')
@@ -49,10 +52,26 @@ class ItemView(TemplateView):
         ).values_list(metric, flat=True).latest('date')
         return metric
     
+    def get_metric_percentage_change(self, metric:str) -> float:
+        prices_objects = Price.objects.filter(
+            item_id=self.item_id
+        ).values_list(metric, flat=True)
+
+        earliest = prices_objects.earliest('date') 
+        if earliest == 0:
+            return 100
+        latest = prices_objects.latest('date')
+
+        percentage_change = (earliest - latest) / earliest * 100
+        return percentage_change
+
     def get_similar_items(self):
         threshold = 75
         selected_item_name = self.get_item_info().item_name
-        item_names = Item.objects.all().values_list('item_name', flat=True).exclude(item_name=selected_item_name)
+        item_names = Item.objects.all().values_list(
+            'item_name', flat=True
+        ).exclude(item_name=selected_item_name)
+
         similar_items = [
             Item.objects.filter(item_name=item_name) 
             for item_name in item_names 
