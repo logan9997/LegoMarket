a4a21db0484bd4d0d11b6392e1565cda40437c6b
logan9997 <80635281+logan9997@users.noreply.github.com>
2023-11-10 18:48:21 +0000

    login form created

--- App/forms.py
+++ App/forms.py
@@ -1,7 +1,29 @@
 from django import forms
-from config import METRICS
+from config import METRICS, ModelsConfig
+from .models import User
 
 class GraphMetricSelect(forms.Form):
     metric_select = forms.ChoiceField(choices=(
         (metric, ' '.join(word.capitalize() for word in metric.split('_'))) for metric in METRICS)
     )
+
+
+class Login(forms.Form):
+    username = forms.CharField(
+        max_length=ModelsConfig.Length.USERNAME
+    )
+    password = forms.CharField(
+        max_length=ModelsConfig.Length.PASSWORD, 
+        widget=forms.PasswordInput()
+    )
+
+    def is_valid(self) -> bool:
+        username = self.data.get('username')
+        password = self.data.get('password')
+        self.user = User.objects.filter(username=username, password=password)
+        if self.user.exists():
+            return True
+        return False
+    
+    def get_user_id(self):
+        return self.user.get().user_id
