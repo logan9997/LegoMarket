ea2dbc51766679e202f4ad67149575f8dfa74614
logan9997 <80635281+logan9997@users.noreply.github.com>
2023-11-11 12:04:11 +0000

    using class structure

--- App/static/App/chart.js
+++ App/static/App/chart.js
@@ -1,61 +1,65 @@
-function get_chart() {
-    var chart_id = JSON.parse(document.getElementById('chart-id').textContent)
-    var chart = document.getElementById(chart_id);
-    return chart
-}
+class CreateChart {
+	get_canvas() {
+		let chart_id = JSON.parse(document.getElementById('chart-id').textContent)
+		let chart = document.getElementById(chart_id);
+		return chart
+	}
 
-function get_data() {
-    var data = JSON.parse(document.getElementById('chart-data').textContent);
-    return data;
-}
+	get_data() {
+		let data = JSON.parse(document.getElementById('chart-data').textContent);
+		return data;
+	}
 
-function get_labels() {
-    var labels = JSON.parse(document.getElementById('chart-labels').textContent);
-    return labels;
-}
+	get_labels() {
+		let labels = JSON.parse(document.getElementById('chart-labels').textContent);
+		return labels;
+	}
 
-function get_gradient() {
-    var gradient = chart.getContext('2d').createLinearGradient(0, 0, 0, 400);
-    gradient.addColorStop(0, 'rgba(250,174,50,1)');   
-    gradient.addColorStop(1, 'rgba(250,174,50,0)');
-    return gradient;
-}
+	get_gradient() {
+		let gradient = this.get_canvas().getContext('2d').createLinearGradient(0, 0, 0, 400);
+		gradient.addColorStop(0, 'rgba(250,174,50,1)');   
+		gradient.addColorStop(1, 'rgba(250,174,50,0)');
+		return gradient;
+	}
 
-function get_options() {
-    options = {
-      scales: {
-        x: {
-          grid: {
-            display: false
-            },
-          display:false
-        },
-        y: {
-          beginAtZero: true,
-            grid: {
-              display: false
-            }
-          }
-      },
-      plugins: {
-        legend: {
-          display: false
-        }
-      },
-      fill :true
-    }
-    return options
-}
+	get_options() {
+		let options = {
+			scales: {
+				x: {
+					grid: {
+						display: false
+						},
+					display:false
+				},
+				y: {
+					beginAtZero: true,
+						grid: {
+							display: false
+						}
+					}
+			},
+			plugins: {
+				legend: {
+					display: false
+				}
+			},
+			fill :true
+		}
+		return options
+	}
 
-new Chart(chart, {
-    type: 'line',
-    data: {
-        labels: get_labels(),
-        datasets: [{
-        data: get_data(),
-            backgroundColor: get_gradient(),
-            borderColor: 'rgb(255, 10, 86,1)',
-        }]
-    },
-    options: get_options()
-});
+	create_chart() {
+		new Chart(this.get_canvas(), {
+			type: 'line',
+			data: {
+				labels: this.get_labels(),
+				datasets: [{
+				data: this.get_data(),
+					backgroundColor: this.get_gradient(),
+					borderColor: 'rgb(255, 10, 86,1)',
+				}]
+			},
+			options: this.get_options()
+		});
+	}
+}
