b27b48c7fb4fb7f1fb00d7336cc760f11cdcfa37
logan9997 <80635281+logan9997@users.noreply.github.com>
2023-11-12 21:03:46 +0000

    search_form configured in urlpatterns

--- App/urls.py
+++ App/urls.py
@@ -1,12 +1,12 @@
 from django.urls import path
 from .views import (
-    home, item, login, signup
+    home, item, login, signup, search_form
 )
 
 urlpatterns = [
     path('', home.home, name='home'),
+    path('search_form', search_form.search_form, name='search_form'),
     path('item/<str:item_id>/', item.ItemView.as_view(), name='item'),
     path('login/', login.LoginView.as_view(), name='login'),
     path('signup/', signup.SignUpView.as_view(), name='signup'),
-
 ]
