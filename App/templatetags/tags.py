4eb7e497db876c67ce1fd7c01da9ba094624f795
logan9997 <80635281+logan9997@users.noreply.github.com>
2023-11-13 21:54:39 +0000

    created item_type_convert tag, same as utils function

--- App/templatetags/tags.py
+++ App/templatetags/tags.py
@@ -1,6 +1,7 @@
 from django import template
 from ..models import Item
 from ..forms import SearchItem
+from utils import item_type_convert as _item_type_convert
 
 register = template.Library()
 
@@ -22,3 +23,8 @@ def get_item_ids():
         'item_id', flat=True
     ).order_by('item_id')
     return list(item_ids)
+
+
+@register.filter
+def item_type_convert(item_type:str, string_method:str):
+    return getattr(_item_type_convert(item_type), string_method)()
