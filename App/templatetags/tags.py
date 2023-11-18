49ee6f4f3a95a5c03ab896900813131af24c7408
logan9997 <80635281+logan9997@users.noreply.github.com>
2023-11-18 16:59:36 +0000

    created metric_convert, and index

--- App/templatetags/tags.py
+++ App/templatetags/tags.py
@@ -1,7 +1,10 @@
 from django import template
 from ..models import Item
 from ..forms import SearchItem
-from utils import item_type_convert as _item_type_convert
+from utils import (
+    item_type_convert as _item_type_convert, 
+    metric_convert as _metric_convert
+)
 
 register = template.Library()
 
@@ -28,3 +31,13 @@ def get_item_ids():
 @register.filter
 def item_type_convert(item_type:str, string_method:str):
     return getattr(_item_type_convert(item_type), string_method)()
+
+
+@register.filter
+def metric_convert(metric: str):
+    return _metric_convert(metric)
+
+
+@register.filter
+def index(iterable, index:int):
+    return iterable[index]
