fdba352ef00fdaea196aadf358c95ac510b67f2a
logan9997 <80635281+logan9997@users.noreply.github.com>
2023-11-21 20:50:27 +0000

    search path configured

--- App/urls.py
+++ App/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 from .views import (
-    home, item, login, signup, search_form
+    home, item, login, signup, search_form, search
 )
 
 urlpatterns = [
     path('', home.home, name='home'),
     path('search_form', search_form.search_form, name='search_form'),
+    path('search/', search.SearchView.as_view(), name='search'),
     path('item/<str:item_id>/', item.ItemView.as_view(), name='item'),
     path('login/', login.LoginView.as_view(), name='login'),
     path('signup/', signup.SignUpView.as_view(), name='signup'),
