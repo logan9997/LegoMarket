c76acdb456c25f526351afb7524f803acc63be27
logan9997 <80635281+logan9997@users.noreply.github.com>
2023-11-26 17:03:07 +0000

    SearchFormHandler as class

--- App/views/search.py
+++ App/views/search.py
@@ -1,15 +1,63 @@
-from django.http import HttpRequest, HttpResponse
+from django import http
+from django.http import HttpRequest, HttpResponse, QueryDict
 from django.shortcuts import HttpResponse, redirect
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, View
 from ..models import Item
 from ..forms import MetricLimits
 from config import ITEMS_PER_PAGE
 from typing import Any
 from django.db.models import Q
 from utils import get_current_page
+import math
 
-def search_form_handler(request: HttpRequest):
-    return redirect('search/')
+class SearchFormHandler(View):
+
+    def dispatch(self, request: HttpRequest, *args: Any, **kwargs: Any) -> HttpResponse:
+        self.request = request
+        return super().dispatch(request, *args, **kwargs)
+
+    def get(self, request: HttpRequest, *args, **kwargs):
+        previous_url_params = self.get_previous_url_params()
+        new_params = self.get_new_params()
+        params = self.join_params(previous_url_params, new_params)
+        params = self.filter_out_params(params)
+        params_string = params.urlencode()
+        return redirect('search/?' + params_string)
+    
+    def get_previous_url_params(self):
+        previous_url = self.request.META['HTTP_REFERER']
+        params = None
+        if '?' in previous_url:
+            params = previous_url.split('?')[1]
+        params = QueryDict(params, mutable=True)
+        return params
+    
+    def params_string(self, params: dict):
+        return '&'.join([f'{k}={v}' for k, v in params.items()])
+
+    def get_new_params(self):
+        metric_limits = MetricLimits(self.request.GET, request=self.request)
+        if metric_limits.is_valid():
+            data:dict = metric_limits.cleaned_data
+            params_string = self.params_string(data)
+            params = QueryDict(params_string, mutable=True)  
+        return params
+
+    def join_params(self, p1: dict, p2: dict):
+        p2.update(p1)
+        params = dict(p2)
+        for k, v in params.items():
+            params[k] = v[0]
+        params = self.params_string(params)
+        params = QueryDict(params)
+        return params
+    
+    def filter_out_params(self, params: QueryDict):
+        new_params = QueryDict(mutable=True)
+        for k, v in params.items():
+            if v != '0':
+                new_params[k] = v
+        return new_params
 
 
 class SearchView(TemplateView):
@@ -17,26 +65,23 @@ class SearchView(TemplateView):
 
     def dispatch(self, request: HttpRequest, *args: Any, **kwargs: Any) -> HttpResponse:
         self.title = 'Search'
+        self.request = request
         self.query = request.GET.get('q', '')
         self.items = self.get_items(self.query)
-        self.current_page = get_current_page(request)
-        self.last_page = round(len(self.items) / ITEMS_PER_PAGE)
-        self.current_page = self.validate_current_page()
-        self.request = request
+        self.last_page = math.ceil(len(self.items) / ITEMS_PER_PAGE)
+        self.current_page = self.validate_current_page(get_current_page(request))
         return super().dispatch(request, *args, **kwargs)
 
     def get_context_data(self, **kwargs) -> dict[str, Any]:
         context = super().get_context_data(**kwargs)
         context.update({
             'title': self.title,
-            'items': self.items[self.current_page * ITEMS_PER_PAGE : (self.current_page + 1) * ITEMS_PER_PAGE],
+            'items': self.items[(self.current_page - 1) * ITEMS_PER_PAGE : self.current_page * ITEMS_PER_PAGE],
             'current_page': self.current_page,
             'query': self.query,
             'last_page': self.last_page,
             'forms': {
-                'filters': MetricLimits(initial={
-                    field: self.request.GET.get(field, 0) for field in MetricLimits().fields
-                })
+                'filters': MetricLimits(request=self.request)
             }
         })
         return context
@@ -50,9 +95,9 @@ class SearchView(TemplateView):
         ).order_by(*orders)
         return items
     
-    def validate_current_page(self) -> None:
+    def validate_current_page(self, current_page) -> int:
         try:
-            current_page = int(self.current_page)
+            current_page = int(current_page)
         except:
             return 1
         if current_page <= 0:
