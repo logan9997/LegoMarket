eb558836049d1435a2fa26eb1d8ae163bef8e01f
logan9997 <80635281+logan9997@users.noreply.github.com>
2023-12-02 12:14:40 +0000

    order method created inside Filter class, get_items method inside SearchView reconfigured, and selects items price info based off of max date

--- App/views/search.py
+++ App/views/search.py
@@ -1,11 +1,11 @@
 from django.http import HttpRequest, HttpResponse, QueryDict
 from django.shortcuts import HttpResponse, redirect
 from django.views.generic import TemplateView, View
-from ..models import Item
-from ..forms import MetricLimits, ItemType, YearReleased
+from ..models import Item, Price
+from ..forms import MetricLimits, ItemType, YearReleased, Order
 from config import ITEMS_PER_PAGE
 from typing import Any
-from django.db.models import Q, F
+from django.db.models import Q, F, Subquery, OuterRef
 from utils import get_current_page
 import math
 from django import forms
@@ -14,6 +14,13 @@ class Filters:
 
     def __init__(self, request: HttpRequest) -> None:
         self.request = request
+
+    def order(self):
+        order = self.request.GET.get('order')
+        print('ORDER!!', order)
+        if order != None:
+            return (f'price__{order}', )
+        return ()
 
     def get_metric_filters(self) -> dict:
         filtered_metrics = {
@@ -45,7 +52,6 @@ class Filters:
     def filters(self) -> dict:
         filters = {}
         get_filter_methods = [m for m in dir(self) if m[:3] == 'get']
-        print(get_filter_methods)
         for method in get_filter_methods:
             filters.update(getattr(self, method)())
         return filters
@@ -55,9 +61,10 @@ class SearchFormHandler(View):
     def dispatch(self, request: HttpRequest, *args: Any, **kwargs: Any) -> HttpResponse:
         self.request = request
         self.forms:dict[str, forms.Form] = {
-            'metric_limits': MetricLimits(self.request.GET, request=self.request),
-            'item_type': ItemType(self.request.GET),
-            'year_released':YearReleased(self.request.GET)
+            'metric_limits': MetricLimits(request.GET, request=self.request),
+            'item_type': ItemType(request.GET),
+            'year_released':YearReleased(request.GET),
+            'order': Order(request.GET)
         }
         return super().dispatch(request, *args, **kwargs)
 
@@ -83,7 +90,6 @@ class SearchFormHandler(View):
     def get_new_params(self):
         form_name = self.request.GET.get('form_name')
         form = self.forms.get(form_name)
-        
         if form.is_valid():
             data:dict = form.cleaned_data
             params_string = self.params_string(data)
@@ -116,7 +122,8 @@ class SearchView(TemplateView):
         self.request = request
         self.query = request.GET.get('q', '')
         self.filters = Filters(request).filters()
-        self.items = self.get_items(self.query, filters=self.filters)
+        self.order = Filters(request).order()
+        self.items = self.get_items(self.query, filters=self.filters, orders=self.order)
         self.last_page = math.ceil(len(self.items) / ITEMS_PER_PAGE)
         self.current_page = self.validate_current_page(get_current_page(request))
         return super().dispatch(request, *args, **kwargs)
@@ -132,7 +139,8 @@ class SearchView(TemplateView):
             'forms': {
                 'filters': MetricLimits(request=self.request),
                 'item_type': ItemType(),
-                'year_released': YearReleased()
+                'year_released': YearReleased(),
+                'order': Order()
             }
         })
         return context
@@ -141,23 +149,28 @@ class SearchView(TemplateView):
         return super().get(request, *args, **kwargs)
     
     def get_items(self, query, orders=('item_id', 'item_type'), filters={}):
-        items = Item.objects.filter(
-            Q(item_id__icontains=query) | Q(item_name__icontains=query), 
-            **filters
-        ).order_by(*orders).values(
-            'item_id',
-            'item_name',
-            'item_type',
-            'image_path',
-            price_used=F('price__price_used'),
-            price_new=F('price__price_new'),
-            qty_used=F('price__qty_used'),
-            qty_new=F('price__qty_new'),
-        ).distinct('item_id')
-        return items
-    
+        latest_date_query = Price.objects.filter(
+            item=OuterRef('item_id')
+        ).order_by('-date').values('date')[:1]
 
-    
+        items = Price.objects.select_related('item').annotate(
+            latest_date=Subquery(latest_date_query)
+        ).values(
+            'price_used',
+            'price_new',
+            'qty_used',
+            'qty_new', 
+            'item_id',
+            item_name=F('item__item_name'),
+            item_type=F('item__item_type'),
+            image_path=F('item__image_path'), 
+            year_released=F('item__year_released')        
+        ).filter(
+            Q(item__item_id__icontains=query) | Q(item_name__icontains=query),
+            date=F('latest_date'), **filters
+        )
+        return items
+
     def validate_current_page(self, current_page) -> int:
         try:
             current_page = int(current_page)
