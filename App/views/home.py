87eb19536b5677939a33f5f5fffea39ceed7e134
logan9997 <80635281+logan9997@users.noreply.github.com>
2023-12-19 11:10:14 +0000

    get_trending_items method gets max date for each item_id

--- App/views/home.py
+++ App/views/home.py
@@ -1,12 +1,9 @@
-from django.shortcuts import render
 from django.http import HttpRequest, HttpResponse
 from django.views.generic import TemplateView
 from typing import Any
 from ..models import Item, Price
-from django.db.models import Subquery, OuterRef, F, DecimalField, Func, Value
-from datetime import datetime
-from config import DATE_FORMAT
-
+from django.db.models import Subquery, OuterRef, F, DecimalField, Func, Value, ExpressionWrapper, Max
+from utils import timer
 
 class HomeView(TemplateView):
     template_name = 'App/home/home.html'
@@ -15,6 +12,7 @@ class HomeView(TemplateView):
         self.request = request
         return super().dispatch(request, *args, **kwargs)
 
+    @timer
     def get_context_data(self, **kwargs) -> dict[str, Any]:
         context = super().get_context_data(**kwargs)
         self.get_trending_items()
@@ -34,25 +32,43 @@ class HomeView(TemplateView):
         return items
     
     def get_trending_items(self):
-        todays_date = datetime.now().strftime(DATE_FORMAT)
-        latest_prices = Price.objects.filter(
-            date=todays_date, item_id=OuterRef('item_id')
-        ).values('price_new')[:1]
-        
-        percentage_change = (
-            (F('price_new') - Subquery(latest_prices)) / F('price_new') * 100
-        )     
+        # get the max 'date' for each item_id
+        latest_dates = Price.objects.filter(
+            item_id=OuterRef('item_id')
+        ).values(
+            'item_id'
+        ).annotate(
+            max_date=Max('date')
+        ).values('max_date')[:1]
 
-        result = Price.objects.filter(date='2023-12-11').values(
-                'item_id', 
-                item_name=F('item__item_name'),
-                image_path=F('item__image_path'),
-            ).annotate(
-                price_new_change=Func(
+        # get 'price_new' for each item where 'date' = 'max_date' from latest_dates query
+        latest_prices = Price.objects.filter(
+            date=Subquery(latest_dates), item_id=OuterRef('item_id')
+        ).values(
+            'price_new'
+        )[:1]
+
+        percentage_change = (F('price_new') - Subquery(latest_prices)) / F('price_new') * -100
+
+        order_by_expression = Func(
+            F('price_change'),
+            function='ABS',
+            output_field=DecimalField()
+        )
+
+        result = Price.objects.filter(date='2023-11-10', price_new__gt=0).values(
+            'item_id',
+            item_name=F('item__item_name'),
+            image_path=F('item__image_path'),
+        ).annotate(
+            price_change=ExpressionWrapper(
+                Func(
                     percentage_change,
                     Value(2),
                     function='ROUND',
                     output_field=DecimalField()
-                )
+                ), 
+                output_field=DecimalField()
             )
-        print(result[0].values())
+        ).order_by(order_by_expression.desc())[:10]
+        return result
