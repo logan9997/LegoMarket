924a12169e699069268d42820c506c51d68de05a
logan9997 <80635281+logan9997@users.noreply.github.com>
2023-12-20 14:22:07 +0000

    portfolio path configured

--- App/urls.py
+++ App/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from .views import (
-    home, item, login, signup, search, 
+    home, item, login, signup, search, portfolio
 )
 
 urlpatterns = [
@@ -8,6 +8,7 @@ urlpatterns = [
     path('search/', search.SearchView.as_view(), name='search'),
     path('search_form_hander/', search.FormHandler.as_view(), name='search_form_hander'),
     path('item/<str:item_id>/', item.ItemView.as_view(), name='item'),
+    path('portfolio/', portfolio.PortfolioView.as_view(), name='portfolio'),
     path('login/', login.LoginView.as_view(), name='login'),
     path('signup/', signup.SignUpView.as_view(), name='signup'),
 ]
