384525dd263c1b4179a8f04092fd7d776b1c774d
logan9997 <80635281+logan9997@users.noreply.github.com>
2024-02-02 22:56:22 +0000

    Input class created

--- config.py
+++ config.py
@@ -1,5 +1,7 @@
 DATE_FORMAT = '%Y-%m-%d'
-METRICS = ['price_new', 'price_used', 'qty_new', 'qty_used']
+SEARCH_ITEMS_PER_PAGE = 18
+MAX_RECENTLY_VIEWED_ITEMS = 8
+NO_USER_LOGGED_IN_VALUE = -1
 
 class ModelsConfig:
 
@@ -10,6 +12,7 @@ class ModelsConfig:
         USERNAME = 16
         PASSWORD = 20
         IMAGE_PATH = 400
+        NOTES = 300
 
     class Decimal:
         MAX_DIGITS = 10
@@ -17,3 +20,15 @@ class ModelsConfig:
 
     class Choice:
         ITEM_TYPE = (('S', 'S'), ('M', 'M'))
+
+
+class Input:
+
+    PAGE_BUTTON_INPUTS = [
+        # {'name':'first', 'value': '<<'},
+        {'name':'previous', 'value': '<', 'direction' : -1},
+        {'name':'next', 'value': '>', 'direction': 1},
+        # {'name':'last', 'value': '>>'},
+    ]
+
+METRICS = ['price_new', 'price_used', 'qty_new', 'qty_used']
