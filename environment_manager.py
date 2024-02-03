3d0181c2625045554748e9e5f69a25d3830997ea
logan9997 <80635281+logan9997@users.noreply.github.com>
2024-02-03 21:01:29 +0000

    .

--- environment_manager.py
+++ environment_manager.py
@@ -15,11 +15,11 @@ class Manager:
         if conn_type == 'psycopg2':
             if DEVELOPMENT:
                 return {
-                    'dbname': os.environ.get('PROD_DB_NAME'),
-                    'user': os.environ.get('PROD_DB_USER'),
-                    'password': os.environ.get('PROD_DB_PASSWORD'),
-                    'host': os.environ.get('PROD_DB_HOST'),
-                    'port': os.environ.get('PROD_DB_PORT'),
+                    'dbname': os.environ.get('DB_NAME'),
+                    'user': os.environ.get('DB_USER'),
+                    'password': os.environ.get('DB_PASSWORD'),
+                    'host': os.environ.get('DB_HOST'),
+                    'port': os.environ.get('DB_PORT'),
                 }
             else:
                 return {
@@ -32,11 +32,11 @@ class Manager:
         elif conn_type == 'settings':
             return {
                 'ENGINE': 'django.db.backends.postgresql',
-                'NAME': os.environ.get('PROD_DB_NAME'),
-                'USER': os.environ.get('PROD_DB_USER'),
-                'PASSWORD': os.environ.get('PROD_DB_PASSWORD'),
-                'HOST': os.environ.get('PROD_DB_HOST'),
-                'PORT': os.environ.get('PROD_DB_PORT'),
+                'NAME': os.environ.get('DB_NAME'),
+                'USER': os.environ.get('DB_USER'),
+                'PASSWORD': os.environ.get('DB_PASSWORD'),
+                'HOST': os.environ.get('DB_HOST'),
+                'PORT': os.environ.get('DB_PORT'),
             }
         else:
             raise Exception(f'Invalid conn_type - ({conn_type}). conn_type must equal "psycopg2" or "settings"') 
